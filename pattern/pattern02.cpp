9f654a700839c6862a96a87795be3c11eeb80b7b
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-11-22 20:57:03 +0530

    dsa with c++

---
+++ pattern/pattern02.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char n ='z';
+    for(char i='a';i<=n;i++){
+        for(char j ='a';j<=n;j++){
+            cout<< j;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
