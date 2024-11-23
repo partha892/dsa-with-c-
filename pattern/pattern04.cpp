4e255f2bce7cfe95e7c84c6a06d7ff9aee44c3b4
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-11-23 08:14:11 +0530

    pattern

---
+++ pattern/pattern04.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+   // int chh = 'a';
+    char ch = 'a';
+   int n = 3;
+    for (int i = 0; i < n; i++)
+    {
+        for(int j = 0;j<n;j++){
+        cout<<ch<<" ";
+        ch++;
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
