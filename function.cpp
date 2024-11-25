ed8dd40637febed48e874ccf9a2380b3c0ce8b34
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-11-25 11:00:09 +0530

    dsa with c++

---
+++ function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void parthaName(){
+    cout<<"partha pratim bhukta \n";
+   
+}
+
+int main (){
+parthaName();
+parthaName();
+parthaName();
+
+    return 0 ;
+}
