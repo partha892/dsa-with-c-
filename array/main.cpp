d776a9e7716c2a44fc54bf773fb3d72d3ab2ae41
Partha Pratim Bhukta <parthapratimbhukta09@gmail.com>
2024-11-30 17:59:53 +0530

    .

--- array/main.cpp
+++ array/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int arr []={2,23,23,34,54,6};
      int n = sizeof(arr) / sizeof(arr[0]); 
-   // cout<<arr[0];
+    cout<<arr[0];
    for(int i =0;i<n;i++){
     cout<<arr[i]<<" ";
    }
